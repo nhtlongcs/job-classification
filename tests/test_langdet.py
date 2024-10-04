bb42b4c291b295c76409c7a5bad0f2cbccd1d639
nhtlongcs <thanglong.nguyenho27@mail.dcu.ie>
2024-10-04 14:14:49 +0100

    test

---
+++ tests/test_langdet.py
@@ -0,0 +1,12 @@
+from nlp.lang import detect_language
+import pytest
+
+
+def test_detect_language():
+    assert detect_language("Hello, world!") == "en"
+    assert detect_language("Hallo, Welt!") == "de"
+    assert detect_language("Bonjour tout le monde!") == "fr"
+    assert detect_language("Olá, mundo!") == "pt"
+    assert detect_language("こんにちは、世界！") == "ja"
+    assert detect_language("안녕하세요, 세계!") == "ko"
+    assert detect_language("你好，世界！") == "zh-cn"
